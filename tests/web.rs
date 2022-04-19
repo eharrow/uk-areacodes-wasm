0e21374b267aad156ce970e3c5b57b6697fff9de
Ewan Harrow <ewan@harrow.org>
2022-04-19 19:34:37 +0100

    test: add missing find_code test

--- tests/web.rs
+++ tests/web.rs
@@ -3,11 +3,13 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use uk_areacodes::api::find_by_code;
+use uk_areacodes_wasm::*;
 use wasm_bindgen_test::*;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn find() {
+    assert_eq!(find_code("01582"), "Luton");
 }
