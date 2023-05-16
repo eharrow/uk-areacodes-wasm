fe9bb9a9c82e7cfe5a5b46017b1fdb29941d55be
Ewan Harrow <ewan@harrow.org>
2023-05-16 17:08:25 +0100

    feature: exposes starts_with_code api

--- tests/web.rs
+++ tests/web.rs
@@ -3,13 +3,21 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
-use uk_areacodes_wasm::find_code;
+
 use wasm_bindgen_test::*;
+
+use uk_areacodes_wasm::{find_code, starts_with_code};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn find() {
     let string = find_code("01582");
+    assert_eq!(string, "Luton");
+}
+
+#[wasm_bindgen_test]
+fn find_by_starts_with() {
+    let string = starts_with_code("01582 12345678");
     assert_eq!(string, "Luton");
 }
