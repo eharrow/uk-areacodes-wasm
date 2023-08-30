f63685f906390ccb1fc74ecad32db8fb4d0c304e
Ewan Harrow <ewan@harrow.org>
2023-08-30 15:26:24 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,6 @@ use uk_areacodes::api::{self};
 use utils::set_panic_hook;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     set_panic_hook();
