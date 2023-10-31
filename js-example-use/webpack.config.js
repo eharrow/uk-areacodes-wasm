8025c96c2975f2fe9f94cb46c92cc3d28e9a65b5
Ewan Harrow <134166+eharrow@users.noreply.github.com>
2023-10-31 13:23:13 +0000

    refactor: add html form to test

--- js-example-use/webpack.config.js
+++ js-example-use/webpack.config.js
@@ -1,14 +1,35 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: ["./bootstrap.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bootstrap.js",
   },
-  mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html'])
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "index.html",
+          context: path.resolve(__dirname),
+        },
+      ],
+    }),
+    new WasmPackPlugin({
+      crateDirectory: path.resolve(__dirname, "."),
+    }),
+    // Have this example work in Edge which doesn't ship `TextEncoder` or
+    // `TextDecoder` at this time.
+    new webpack.ProvidePlugin({
+      TextDecoder: ["text-encoding", "TextDecoder"],
+      TextEncoder: ["text-encoding", "TextEncoder"],
+    }),
   ],
+  mode: "development",
+  experiments: {
+    asyncWebAssembly: true,
+  },
 };
