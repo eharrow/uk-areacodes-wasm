5eaf10367a48dc667e41d1c8a093d4dfec36252a
Ewan Harrow <134166+eharrow@users.noreply.github.com>
2023-11-29 20:25:09 +0000

    fix: listen for enter key to run a search and restrict input field to numbers only

--- js-example-use/index.js
+++ js-example-use/index.js
@@ -4,8 +4,21 @@ import * as wasm from "uk-areacodes-wasm";
 document.querySelector("button").addEventListener("click", () => {
   lookup();
 });
+// Get the input field
+const input = document.getElementById("place");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.querySelector("button").click();
+  }
+});
 function lookup() {
-  const p = document.getElementById("place").value.trim();
+  const p = input.value.trim();
   let res = wasm.find_code(p);
   document.getElementById("target").innerHTML = res !== "" ? `🎉 ${res}` : "🙁 No match";
 }
