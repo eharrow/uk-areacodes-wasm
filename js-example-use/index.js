09e3f0fe9e8b4fba8754ca55d5d6e03df4ddf1f3
Ewan Harrow <134166+eharrow@users.noreply.github.com>
2025-10-03 18:03:17 +0100

    Use latlon (#229)
    * feat: add map
    
    * refactor:
    
    remove commented out code

--- js-example-use/index.js
+++ js-example-use/index.js
@@ -1,5 +1,14 @@
 import * as wasm from "uk-areacodes-wasm";
 
+let marker;
+let circle;
+const CENTRAL_UK = [53.7974185, -1.5437941];
+const map = L.map('map').setView([53.7974185, -1.5437941], 5);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 // console.log(wasm.find_code("01582"));
 document.querySelector("button").addEventListener("click", () => {
   lookup();
@@ -17,8 +26,27 @@ input.addEventListener("keypress", function(event) {
     document.querySelector("button").click();
   }
 });
+
 function lookup() {
   const p = input.value.trim();
-  let res = wasm.find_code(p);
-  document.getElementById("target").innerHTML = res !== "" ? `🎉 ${res}` : "🙁 No match";
+  let res = wasm.find_place_by_code(p);
+
+  // cleanup the map before adding markers for a new place
+  if (marker) marker.remove();
+  if (circle) circle.remove();
+
+  if(res.area !== "") {
+    document.getElementById("target").innerHTML = `🎉 ${res.area}`;
+    map.setView([res.lat, res.lon], 9);
+    marker = L.marker([res.lat, res.lon]).addTo(map);
+    circle = L.circle([res.lat, res.lon], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 7000
+    }).addTo(map);
+  } else {
+    document.getElementById("target").innerHTML = "🙁 No match";
+    map.setView(CENTRAL_UK, 5);
+  }
 }
